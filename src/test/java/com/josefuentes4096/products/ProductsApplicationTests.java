adb47e7e0fd8bb07585a453ac7d581ded6e4c915
Jose Fuentes <josefuentes4096@gmail.com>
2026-03-29 21:08:15 -0300

    Creado con https://start.spring.io/, dependencias: Spring Web, Spring Data JPA, Spring Security, SpringDoc OpenAPI, MySQL Driver

---
+++ src/test/java/com/josefuentes4096/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josefuentes4096.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
